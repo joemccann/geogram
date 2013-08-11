312420d02b8784b2a7f4b9002218c58aab9938b7
joemccann <joseph.isaac@gmail.com>
2013-08-11 09:34:23 -0400

    refactored geogram module

--- plugins/instagram/instagram.js
+++ plugins/instagram/instagram.js
@@ -6,49 +6,72 @@ var fs = require('fs')
 // TODO: REFECTOR WITH STREAM/NON-BLOCKING
 var instagram_config = JSON.parse( fs.readFileSync( path.resolve(__dirname, 'instagram-config.json'), 'utf-8' ) )
 
-function toUTC(dateString){
+
+/**
+ * Constructor
+ */
+var Geogram = function(){}
+
+/**
+ * Returns a Unix Timestamp for a date string.
+ * @param {String} dateString, a calendar date in string format
+ */
+Geogram.prototype._toUTC = function(dateString){
   return new Date(dateString).getTime() / 1000
 }
 
-
-var Instagram = {}
-
-Instagram.geogram = {
-  executeGeoSearch: function(req,res){
-
-    var lat = req.body.latitude || 40.762485
-      , lng = req.body.longitude || -73.997513
-      , minTimestamp = toUTC( req.body.minUTC ) || 1375315200
-      , maxTimestamp = toUTC( req.body.maxUTC ) || 1375920000
-      , distance = req.body.distance || 35
-
-    // console.log(lat + " is the latiude.")
-    // console.log(lng + " is the longitude.")
-    // console.log(minTimestamp + " is the minUTC.")
-    // console.log(maxTimestamp + " is the maxUTC.")
-    // console.log(distance + " is the distance.")
-
-    var config = {
-      method: 'GET'
-      , uri: "https://api.instagram.com/v1/media/search?lat="
-              +lat+"&lng="
-              +lng+"&client_id="
-              +instagram_config.client_id+"&distance="
-              +distance+"&min_timestamp="
-              +minTimestamp+"&max_timestamp="+maxTimestamp
-    }
-
-    return request(config,function(e,r,b){
-      if(e){
-        return res.status(r.statusCode).send(e.error.message)
-      }
-      else {
-
-        return res.json(b)
-      }
-    })
-  } // end executeGeoSearch
-  
+/**
+ * Returns a string that is the geo-search URL
+ * @param {String} lat, the latitude to search
+ * @param {String} lng, the longitude to search
+ * @param {String} minTimestamp, the beginning of the duration as a Unix Timestamp
+ * @param {String} maxTimestamp, the end of the duration as a Unix Timestamp
+ * @param {String} distance, the radius to search in meters
+ */
+Geogram.prototype._buildGeoSearchUri = function(lat,lng,minTimestamp,maxTimestamp,distance){
+  return "https://api.instagram.com/v1/media/search?lat="
+            +lat+"&lng="
+            +lng+"&client_id="
+            +instagram_config.client_id+"&distance="
+            +distance+"&min_timestamp="
+            +minTimestamp+"&max_timestamp="+maxTimestamp
 }
 
-exports.Instagram = Instagram
+
+/**
+ * Executes a geo-location instagram search
+ * @param {Request Object} req, the incoming request object
+ * @param {Request Object} req, the outgoing response object
+ * @param {Function} cb, callback to be executed
+ */
+Geogram.prototype.executeGeoSearch = function(req,res,cb){
+  var lat = req.body.latitude || 40.762485
+    , lng = req.body.longitude || -73.997513
+    , minTimestamp = this._toUTC( req.body.minUTC ) || 1375315200
+    , maxTimestamp = this._toUTC( req.body.maxUTC ) || 1375920000
+    , distance = req.body.distance || 35
+
+  // console.log(lat + " is the latiude.")
+  // console.log(lng + " is the longitude.")
+  // console.log(minTimestamp + " is the minUTC.")
+  // console.log(maxTimestamp + " is the maxUTC.")
+  // console.log(distance + " is the distance.")
+
+  var config = {
+    method: 'GET'
+    , uri: this._buildGeoSearchUri(lat,lng,minTimestamp,maxTimestamp,distance)
+  }
+
+  return request(config,function(error,response,body){
+    if(error){
+      console.error(e)
+      return cb(e)
+    }
+    else {
+      cb(null,body)
+    }
+  })
+}
+
+
+module.exports = Geogram
--- routes/instagram/index.js
+++ routes/instagram/index.js
@@ -1,22 +1,9 @@
 var path = require('path')
-var Instagram = require(
+var Geogram = require(
 	path.resolve(
-		__dirname, '..', '..', 'plugins/instagram/instagram.js')).Instagram
+		__dirname, '..', '..', 'plugins/instagram/instagram.js'))
 
-
-exports.instagram_get_next_page_of_instagram_photos = function(req,res){
-
-  if(!req.session.instagram) return res.redirect('/instagram')
-
-  if(!req.query.next_page_url) {
-    res.type('text/plain')
-    return res.status(403).send("Bad Request. Missing next_page_url param")
-  }
-  
-  Instagram.Geogram.getNextPageOfInstagramPhotos(req,res)
-  
-}
-
+var geogram = new Geogram()
 
 exports.search_geo_post = function(req,res){
 
@@ -25,6 +12,9 @@ exports.search_geo_post = function(req,res){
     return res.status(403).send("Bad Request. You need a latitude and longitude.")
   }
   
-  Instagram.geogram.executeGeoSearch(req,res)
+  geogram.executeGeoSearch(req,res,function(err,data){
+    if(err) return res.status(500).json(err)
+    return res.json(data)  
+  })
   
 }
