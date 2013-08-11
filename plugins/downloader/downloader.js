070556cb3f0b15be1332999f891cc931fe083bad
joemccann <joseph.isaac@gmail.com>
2013-08-11 10:29:00 -0400

    added downloader capabilities

---
+++ plugins/downloader/downloader.js
@@ -0,0 +1,85 @@
+var fs = require('fs')
+	, url = require('url')
+	, path = require('path')
+	, http = require('http')
+
+
+/**
+ * Constructor
+ * @param {String} folderPath, the path where to save the files
+ */
+var Downloader = function(folderPath){
+	this.folderPath = folderPath
+}
+
+
+/**
+ * Cycles through an array of images and downloads them
+ * @param {Object} json, JSON response from Instagram of array of images
+ * @param {Function} cb, callback function
+ */
+Downloader.prototype.downloadSetOfFiles = function(json, cb){
+
+	var self = this
+
+	if(!json || !json.data) return cb(new Error('No JSON data.'))
+
+	if(!json.data.length) return cb(new Error('No images to download.'))
+
+	// Iterate over each one and download
+	json.data.forEach(function dowloadSetForeachCb(el){
+		self.downloadFile( el.images.standard_resolution.url )
+	})	
+
+} // end downloadSetOfFiles
+
+
+// Thanks @rogeriopvl https://github.com/rogeriopvl/downstagram/blob/master/lib/downstagram.js#L70
+
+/**
+ * Downloads a photo with a given URL
+ * @param {String} fileURL, the URL of the photo to download
+ * @param {String} folderSuffix, the folder name to append to main download folder
+ */
+Downloader.prototype.downloadFile = function(fileURL, folderSuffix){
+    
+    var options = {
+        host: url.parse(fileURL).host,
+        port: 80,
+        path: url.parse(fileURL).pathname
+    }
+
+    var folderPath = path.join(this.folderPath, (folderSuffix || 'random_instagram_photos') )
+
+    if (!fs.existsSync(folderPath)){
+        fs.mkdirSync(folderPath)
+    }
+
+    var fileName = url.parse(fileURL).pathname.split('/').pop()
+    	,	file = fs.createWriteStream(folderPath + '/' + fileName)
+    	,	self = this
+    	,	req = http.get(options, function(res){
+
+        var content = ''
+        	;
+        
+        res.on('data', function(chunk){
+            file.write(chunk)
+        })
+        
+        res.on('end', function(chunk){
+            file.end()
+        })
+
+    }) // end http.get()
+
+    req.on('error', function(e){
+        console.log('Error on request: ' + e);
+    })
+
+    return req.end()
+    
+}
+
+
+module.exports = Downloader
--- routes/instagram/index.js
+++ routes/instagram/index.js
@@ -1,9 +1,9 @@
 var path = require('path')
-var Geogram = require(
-	path.resolve(
-		__dirname, '..', '..', 'plugins/instagram/instagram.js'))
+var Geogram = require(path.resolve(__dirname, '..', '..', 'plugins/instagram/instagram.js'))
+  , Downloader = require(path.resolve(__dirname, '..', '..', 'plugins/downloader/downloader.js'))
 
 var geogram = new Geogram()
+  , downloader = new Downloader(path.resolve(__dirname, '..', '..', 'public/downloads/'))
 
 exports.search_geo_post = function(req,res){
 
@@ -12,9 +12,18 @@ exports.search_geo_post = function(req,res){
     return res.status(403).send("Bad Request. You need a latitude and longitude.")
   }
   
-  geogram.executeGeoSearch(req,res,function(err,data){
+  // Execute the geosearch...
+  geogram.executeGeoSearch(req,res,function executeGeoSearchCallback(err,data){
     if(err) return res.status(500).json(err)
+    else{
+    // Let's download the files
+    downloader.downloadSetOfFiles(JSON.parse(data), function(err,data){
+      if(err) return console.error(err)
+      else console.log(data)
+    })
+    // And send the response back
     return res.json(data)  
-  })
+    }  
+  }) // end executeGeoSearch
   
 }
